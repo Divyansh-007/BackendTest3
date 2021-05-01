05d021c6c14f8d6b35676057c71e9be36a4b522a
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-05-01 19:10:33 +0530

    Set up git repository and fire up the express server

---
+++ index.js
@@ -0,0 +1,26 @@
+// required libraries
+const express = require('express');
+
+const port = 8000;
+
+const app = express();
+
+// // connecting database
+// const db = require('./config/mongoose');
+
+// // for authentication using passport.js
+// const passport = require('passport');
+// // jwt strategy
+// const passportJwt = require('./config/passport-jwt-strategy');
+
+// // to use encoded input data
+// app.use(express.urlencoded());
+
+// // use (main) express router
+// app.use('/',require('./routes/index'));
+
+app.listen(port,function(err){
+    if(err){console.log(`Error in running the server: ${err}`);}
+
+    console.log(`Express server is up and running on port: ${port}`);
+});
