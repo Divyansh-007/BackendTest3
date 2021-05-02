600c43478953fc46a0e94ac6bcecd00537d9e382
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-05-02 16:07:20 +0530

    Create /reports/:status route

--- controllers/api/home_api.js
+++ controllers/api/home_api.js
@@ -1,5 +1,19 @@
+const Report = require('../../models/report');
+
 module.exports.index = function(req,res){
     return res.status(200).json({
         message: 'Welcome to Hospital API'
     });
 }
+
+module.exports.allReports = async function(req,res){
+    let reports = await Report.find({status: req.params.status}).populate('of','name').populate('createdBy','name');
+
+    return res.status(200).json({
+        message: `Reports of all Patients with status ${req.params.status}`,
+        data: {
+            reports: reports
+        }
+    });
+}
+
--- routes/api/v1/home.js
+++ routes/api/v1/home.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const homeAPI = require('../../../controllers/api/home_api');
 
 router.get('/',homeAPI.index);
+router.get('/reports/:status',homeAPI.allReports);
 
 module.exports = router;
